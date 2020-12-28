d6fbe1253565275c4246e9d21be6a534ae8179bd
Ha Van Thanh <thanhhv3@vng.com.vn>
2020-12-28 12:36:21 +0700

    add interceptors

---
+++ src/common/interceptors/request.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthorFilterInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const currentUser = request.user;
+    const { idForeignKey } = currentUser;
+
+    const filterObj = { [idForeignKey]: { eq: currentUser.id } };
+    request.NESTJSX_PARSED_CRUD_REQUEST_KEY.parsed.filter.push({
+      field: `${currentUser.idForeignKey}`,
+      operator: 'eq',
+      value: currentUser.id
+    });
+    request.NESTJSX_PARSED_CRUD_REQUEST_KEY.parsed.search.$and.push(filterObj);
+    return next.handle();
+  }
+}
